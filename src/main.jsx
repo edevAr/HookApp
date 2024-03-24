77bebaa05e103cde253aa574aef9b6e36396750b
Eden Guzman Andia <eden.guzman@niceincontact.com>
2024-03-24 13:32:56 -0400

    Adding useStateHook

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { HooksApp } from './HooksApp'
+import { CounterApp } from './01-useState/CounterApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CounterApp />
   </React.StrictMode>,
 )
