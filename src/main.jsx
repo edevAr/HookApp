28da90314a8488d449f522ca4851393d63151223
Eden Guzman Andia <eden.guzman@niceincontact.com>
2024-03-29 16:36:06 -0400

    Adding useMemo

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,16 @@ import { HooksApp } from './HooksApp'
 import { CounterApp } from './01-useState/CounterApp'
 import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import { SimpleForm } from './02-useEffect/SimpleForm'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+//import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 import { FocusScreen } from './04-useRef/FocusScreen'
 import { Layout } from './05-useLayoutEffect/Layout'
 import { Memorize } from './06-memos/Memorize'
+import { MemoHook } from './06-memos/MemoHook'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Memorize  />
+    <MemoHook  />
   </React.StrictMode>,
 )
