d492715a7b393d78a600b94eeb522ea78302113a
Eden Guzman Andia <eden.guzman@niceincontact.com>
2024-03-29 16:54:38 -0400

    Adding useCallback

---
+++ src/06-memos/CallbackHook.jsx
@@ -0,0 +1,25 @@
+import React, { useCallback, useState } from 'react'
+import { ShowIncrement } from './ShowIncrement'
+
+export const CallbackHook = () => {
+    const [counter, setCounter] = useState(10)
+
+    const incrementFather  = useCallback(
+      () => {
+        setCounter((value) => value +1)
+      },
+      [],
+    )
+    
+
+    /*const incrementFather = () => {
+        setCounter(counter+1)
+    }*/
+  return (
+    <>
+        <div>useCallback Hook: {counter}</div>
+        <hr />
+        <ShowIncrement increment={ incrementFather }/>
+    </>
+  )
+}
