66d3f6272f71170f413d62b6aa820b4112234f81
Eden Guzman Andia <eden.guzman@niceincontact.com>
2024-03-29 16:16:58 -0400

    Adding memo hook

--- src/06-memos/Smal.jsx
+++ src/06-memos/Smal.jsx
@@ -1,7 +1,8 @@
+import {memo} from 'react'
 
-
-export const Smal = ({value}) => {
+export const Smal = memo(({value}) => {
+    console.log('me volvi a dibujar :( ');
   return (
     <small>{value}</small>
   )
-}
+})
