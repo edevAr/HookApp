474375cd0354856679b53b466761231939e45493
Eden Guzman Andia <eden.guzman@niceincontact.com>
2024-03-31 18:31:54 -0400

    Changing todoToggle feature

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -33,13 +33,23 @@ export const TodoApp = () => {
             payload: id
         })
     }
+    const onToggleTodo = (id) => {
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        })
+    }
   return (
     <>
         <h1>TodoApp: 10, <small>pendientes: 2</small></h1>
         <hr />
         <div className='row'>
             <div className='col-7'>
-                <TodoList todos={todos} onDeleteTodo={ id => handleDeleteItem(id)}/>
+                <TodoList 
+                    todos={todos} 
+                    onDeleteTodo={ id => handleDeleteItem(id)}
+                    onToggleTodo = {onToggleTodo}
+                />
             </div>
             <div className='col-5'>
                 <h4>Agregar TODO</h4>
