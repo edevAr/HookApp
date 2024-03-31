9a7496e67a7614a927bf2fd7281d2f78a7b20b29
Eden Guzman Andia <eden.guzman@niceincontact.com>
2024-03-31 17:42:34 -0400

    Adding functionlaity to Todolist

--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -2,9 +2,8 @@
 
 export const todoReducer = (initialState =[], action) => {
     switch (action.type) {
-        case 'ABC':
-            throw new Error('Action.type = ABC no esta implementada')
-            //return initialState
+        case '[TODO] Add Todo':
+            return [...initialState, action.payload]
         default:
             return initialState
     }
