30de42b022de8d169b9022fb44a77ce93a871ceb
Eden Guzman Andia <eden.guzman@niceincontact.com>
2024-03-31 17:53:43 -0400

    Adding local storage to read and write locally

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,22 +1,24 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { todoReducer } from './todoReducer'
 import { TodoList } from './TodoList'
 import { TodoAdd } from './TodoAdd'
 
 export const TodoApp = () => {
     const initialState = [
-        {
-            id: new Date().getTime(),
-            description: 'Recolectar la piedra del alma',
-            done: false
-        },
-        {
-            id: new Date().getTime()*3,
-            description: 'Recolectar la piedra del infinito',
-            done: false
-        }
     ]
-    const [todos, dispatch] = useReducer(todoReducer, initialState)
+    const init = () => {
+        return JSON.parse(localStorage.getItem('todos')) ||[]
+    }
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init)
+    
+    useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    
+      
+    }, [todos])
+    
+
     const handleNewTodo = (todo) => {
         const action = {
             type: '[TODO] Add Todo',
