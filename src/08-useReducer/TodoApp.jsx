c45bc0fdb0c775d3e81cd66024d906602be5c1dd
Eden Guzman Andia <eden.guzman@niceincontact.com>
2024-03-31 18:13:18 -0400

    Adding feature to delete

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -27,13 +27,19 @@ export const TodoApp = () => {
         dispatch(action);
 
     }
+    const handleDeleteItem = (id) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        })
+    }
   return (
     <>
         <h1>TodoApp: 10, <small>pendientes: 2</small></h1>
         <hr />
         <div className='row'>
             <div className='col-7'>
-                <TodoList todos={todos}/>
+                <TodoList todos={todos} onDeleteTodo={ id => handleDeleteItem(id)}/>
             </div>
             <div className='col-5'>
                 <h4>Agregar TODO</h4>
