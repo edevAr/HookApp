8f694c0a91aef5c8aa63a4cf1366443fee824a23
Eden Guzman Andia <eden.guzman@niceincontact.com>
2024-03-31 23:22:57 -0400

    Adding useState hook

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './context/UserContext'
 
 export const LoginPage = () => {
+  const {user, setUser} = useContext(UserContext)
+  //console.log(hola, user);
   return (
     <>
         <h1>LoginPage</h1>
         <hr />
+        <pre>
+          {JSON.stringify(user, null, 3)}
+        </pre>
+
+        <button 
+          className='btn btn-primary'
+          onClick={() =>setUser({
+            id: 123,
+            name :'Eden Guzman',
+            email : 'eden.guzman@gmail.com'
+          })}
+        >
+          Establecer Usuario
+        </button>
     </>
   )
 }
--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,15 +1,16 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import { UserContext } from './UserContext'
 
-const user = {
+/*const user = {
     id: 123,
     name :'Eden Guzman',
     email : 'eden.guzman@gmail.com'
-}
+}*/
 export const UserProvider = ({children}) => {
+    const [user, setUser] = useState()
   return (
-    <UserContext.Provider value={{hola: 'Mundo', user: user}}>
+    <UserContext.Provider value={{user, setUser}}>
         {children}
     </UserContext.Provider>
   )
